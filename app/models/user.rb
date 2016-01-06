84d4f95a95eec58f125fc48af73271be92ce2026
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-01-06 00:12:43 +0000

    Added configuration for carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
 end
