1a70cd825be3416c17f8ecc049f85eee1a687ae0
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-01-06 00:05:21 +0000

    Added an Avatar to the user table
    Needed for accessing the url of the avatar for fans and artists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104160608) do
+ActiveRecord::Schema.define(version: 20160105210238) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160104160608) do
     t.text     "bio"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
