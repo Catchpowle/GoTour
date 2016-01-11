23472c2b5a1b13a5d29df19adff5af0695cd2fc7
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-01-11 20:16:59 +0000

    Added geocode functionality to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ActiveRecord::Base
   validates_integrity_of  :avatar
   validates_processing_of :avatar
 
+  geocoded_by :geocode_string
+  before_save :geocode, :if => Proc.new { |profile| profile.city_changed? || profile.country_changed? }
+
+  def geocode_string
+    "#{city}, #{country}"
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -17,5 +24,4 @@ class User < ActiveRecord::Base
       user.avatar = auth.info.image # assuming the user model has an image
     end
   end
-
 end
