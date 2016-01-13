259bb72709743353d7579ee71d9380fd38d8a022
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-01-13 14:04:16 +0000

    Added functionality for displaying artists on a fans page

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -2,6 +2,7 @@ class FansController < ApplicationController
 
   def show
     @fan = Fan.find(params[:id])
+    @other_artist_list = Artist.where(id: current_fan.other_requested_artists.map(&:id))
   end
 
 end
--- app/models/fan.rb
+++ app/models/fan.rb
@@ -3,4 +3,19 @@ class Fan < User
   has_many :artists, through: :requests
 
   devise :omniauthable, :omniauth_providers => [:facebook]
+
+  def other_requested_artists
+    other_artist_list = []
+    Artist.all.each do |a|
+      if self.artists.exclude?(a)
+        a.fans.each do |f|
+          if f.geocode_string == self.geocode_string
+            other_artist_list << a
+            break
+          end
+        end
+      end
+    end
+    other_artist_list
+  end
 end
