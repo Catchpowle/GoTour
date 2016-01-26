3bc22c59184e75ef64d8cc934831ede95822db92
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2016-01-26 23:37:43 +0000

    Updated carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                => 'eu-west-1'
+    :region                => 'us-west-2'
     }
   config.storage = :fog
   config.cache_dir = "#{Rails.root}/tmp/uploads"
